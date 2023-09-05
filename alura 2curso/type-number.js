2cb78c99953beb34433c5b6b8f9300b7677c9e57
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-09-05 09:42:26 -0300

    Add files via upload

---
+++ alura 2curso/type-number.js
@@ -0,0 +1,10 @@
+// tipo number
+
+const meunumero = 3;
+
+const primeironumero = 1;
+const segundonumero = 2;
+
+const operacaomatematica = primeironumero * segundonumero;
+
+consle.log(operacaomatematica)
