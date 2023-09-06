fce673a0293710461a8dd6fe56fa2807dd0e522b
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-09-06 08:41:38 -0300

    Add files via upload

---
+++ jsvariavesfuncoes-main/alura 2curso/type-boolean.js
@@ -0,0 +1,8 @@
+//boolean
+// treue -> falso
+// false -> falso
+
+const primeironumero = 5;
+const segundonumero = 10;
+
+console.log(primeironumero === segundonumero);
---
+++ jsvariavesfuncoes-main/alura 2curso/type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "ola, mundo";
+const texto2 = 'ola, mundo';
+const senha = "senhasupersegura456!";
+const stringdenumeros = "34567";
+
+const citacao = "meu nome e ";
+const meunome = "leonardo";
+
+// concatencao (+)
+
+console.log(citacao + meunome)
+
+//template string ou template literal
